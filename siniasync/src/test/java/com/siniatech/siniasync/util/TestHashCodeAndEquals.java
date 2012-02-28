bb911056348798690cb1fe4202ba3e1fa5380958
Si Fraser <simon@siniatech.com>
2012-02-28 19:49:19 +0000

    get jacoco coverage working after issues using cobetura with Java 7

---
+++ siniasync/src/test/java/com/siniatech/siniasync/util/TestHashCodeAndEquals.java
@@ -0,0 +1,103 @@
+package com.siniatech.siniasync.util;
+
+import static com.siniatech.siniautils.test.AssertHelper.*;
+import static junit.framework.Assert.*;
+
+import java.lang.reflect.Constructor;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.siniatech.siniasync.change.FileContentsChange;
+import com.siniatech.siniasync.change.FileMissingChange;
+import com.siniatech.siniasync.change.FileTypeChange;
+import com.siniatech.siniautils.fn.IFunction0;
+import com.siniatech.siniautils.fn.IResponse1;
+
+public class TestHashCodeAndEquals {
+
+    @SuppressWarnings("rawtypes")
+    private static List<Class> classesToTest = Arrays.<Class> asList( //
+        FileMissingChange.class, //
+        FileTypeChange.class, //
+        FileContentsChange.class //
+        );
+
+    @SuppressWarnings("rawtypes")
+    private static Map<Class, IFunction0> mockClasses = new HashMap<Class, IFunction0>();
+
+    static {
+        mockClasses.put( Path.class, new IFunction0<Path>() {
+            public Path apply() {
+                return FileSystems.getDefault().getPath( "f" );
+            }
+        } );
+    }
+
+    private void visitClasses( IResponse1<Object> response ) throws Exception {
+        for ( Class<?> clazz : classesToTest ) {
+            response.respond( instantiate( clazz ) );
+        }
+    }
+
+    private Object[] instantiate( Class<?>[] parameterTypes ) throws Exception {
+        Object[] parameters = new Object[parameterTypes.length];
+        for ( int i = 0; i < parameterTypes.length; i++ ) {
+            parameters[i] = instantiate( parameterTypes[i] );
+        }
+        return parameters;
+    }
+
+    private Object instantiate( Class<?> clazz ) throws Exception {
+        if ( mockClasses.containsKey( clazz ) ) {
+            return mockClasses.get( clazz ).apply();
+        } else {
+            Constructor<?> constructor = clazz.getConstructors()[0];
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+            return constructor.newInstance( instantiate( parameterTypes ) );
+        }
+    }
+
+    @Test
+    public void testEqualsNull() throws Exception {
+        visitClasses( new IResponse1<Object>() {
+            public void respond( Object t ) {
+                assertFalse( t.equals( null ) );
+            }
+        } );
+    }
+
+    @Test
+    public void testEqualsIdentity() throws Exception {
+        visitClasses( new IResponse1<Object>() {
+            public void respond( Object t ) {
+                assertTrue( t.equals( t ) );
+            }
+        } );
+    }
+
+    @Test
+    public void testEqualsOtherClass() throws Exception {
+        visitClasses( new IResponse1<Object>() {
+            public void respond( Object t ) {
+                assertFalse( t.equals( 1 ) );
+            }
+        } );
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+        visitClasses( new IResponse1<Object>() {
+            public void respond( Object t ) {
+                assertNotEquals( 0, t.hashCode() );
+                assertNotEquals( 1, t.hashCode() );
+                assertEquals( t.hashCode(), t.hashCode() );
+            }
+        } );
+    }
+}
