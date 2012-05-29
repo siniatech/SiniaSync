c33039bb1e743c93a15ffe248f5e0290a86df847
si <si@sifraser.co.uk>
2012-05-29 13:21:59 +0100

    license/headers

--- siniasync/src/test/java/com/siniatech/siniasync/util/TestHashCodeAndEquals.java
+++ siniasync/src/test/java/com/siniatech/siniasync/util/TestHashCodeAndEquals.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * SiniaSync
+ * Copyright (c) 2011-2 Siniatech Ltd  
+ * http://www.siniatech.com/products/siniasync
+ *
+ * All rights reserved. This project and the accompanying materials are made 
+ * available under the terms of the MIT License which can be found in the root  
+ * of the project, and at http://www.opensource.org/licenses/mit-license.php
+ *
+ ******************************************************************************/
 package com.siniatech.siniasync.util;
 
 import static com.siniatech.siniautils.test.AssertHelper.*;
